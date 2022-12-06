82328767c964af93817f7bf83797b34289f93503
arimkim <kal72222222@gmail.com>
2022-12-06 11:53:18 +0900

    feat: setup initial server setting and upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+const express = require('express'); 
+const app = express(); 
+const port = 3000; 
+const path = require('path'); 
+app.use(express.static('public'));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); 
+app.set('views', path.join(__dirname, 'views')); 
+
+app.get('/', (req, res) => {
+    res.send("시작 페이지~");  
+}); 
+
+
+app.listen(port, () => {
+    console.log("server is 실행중~"); 
+}); 
