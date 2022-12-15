8587df3d2f17094f56780901e08bc7ca3ea0eadb
arimkim <kal72222222@gmail.com>
2022-12-15 19:49:51 +0900

    feat: add fucntion which octal number to decimal
    Add fucntion which change octal number to decimal.
    And add css style file.
    
    when octal number is inputed, and submit button clicked, user can get decimal number.

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('index' ,  
-    { binTodeci : ' '}); 
+    { binTodeci : ' ', octalTodeci : ' '}); 
 }); 
 
 app.post('/inputBinary', function(req, res){
@@ -30,7 +30,7 @@ app.post('/inputBinary', function(req, res){
 
     if(flag) {
         // res.send("받은값 : " + binary + "\n변환값 : " + decimal); // 사용자에게 지정한 값을 응답해줌
-        res.render('index', { binTodeci : decimal}); 
+        res.render('index', { binTodeci : decimal, octalTodeci :' '}); 
 
 
     }else {
@@ -38,6 +38,31 @@ app.post('/inputBinary', function(req, res){
     }
 
 });
+
+
+app.post('/inputOctal', function(req, res){
+
+    var flag = true; 
+    var octal = req.body.octal;
+    var decimal = parseInt(octal, 8);
+
+    for(var i = 0; i < octal.length; i++) {
+        if((0 >(String(octal).charAt(i)) || ( 8 < (String(octal).charAt(i))))){
+            console.log(String(octal).charAt(i));
+            flag = false;
+            break;  
+        }
+    }
+
+    if(flag) {
+        res.render('index', { binTodeci : ' ', octalTodeci : decimal}); 
+
+    }else {
+        res.send("입력값이 8진수가 아닙니다" ); // 사용자에게 지정한 값을 응답해줌
+    }
+
+});
+
 
 app.listen(port, () => {
     console.log("server is 실행중~"); 
